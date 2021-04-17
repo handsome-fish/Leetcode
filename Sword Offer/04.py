f635393702f3c0b7ce35066d6e12dedf22dced82
handsome-fish <gitfish@163.com>
2021-04-17 17:27:00 +0800

    linear search

--- Sword Offer/04.py
+++ Sword Offer/04.py
@@ -30,6 +30,39 @@
 
 链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
 """
+from typing import List
+
 
 class Solution:
     def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0:
+            return False
+        if len(matrix[0]) == 0:
+            return False
+        i, j = len(matrix) - 1, 0
+        print(len(matrix))
+        print(len(matrix[0]))
+        while i >= 0 and j <= len(matrix[0]) - 1:
+            if target < matrix[i][j]:
+                i -= 1
+                continue
+            elif target > matrix[i][j]:
+                j += 1
+                continue
+            else:
+                return True
+        return False
+
+
+# mat, tar = [
+#                [1, 4, 7, 11, 15],
+#                [2, 5, 8, 12, 19],
+#                [3, 6, 9, 16, 22],
+#                [10, 13, 14, 17, 24],
+#                [18, 21, 23, 26, 30]
+#            ], 30
+
+mat, tar = [[-1,3]], 3
+
+s = Solution()
+print(s.findNumberIn2DArray(matrix=mat, target=tar))
