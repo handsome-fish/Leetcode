5107a533d650b4ac12e33b9a4c46df0d7563c028
handsome-fish <gitfish@163.com>
2021-04-22 23:24:48 +0800

    dfs

---
+++ Sword Offer/12.py
@@ -0,0 +1,52 @@
+"""
+剑指 Offer 12. 矩阵中的路径
+
+给定一个m x n 二维字符网格board 和一个字符串单词word 。如果word 存在于网格中，返回 true ；否则，返回 false 。
+
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+
+
+示例 1：
+
+输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+输出：true
+示例 2：
+
+输入：board = [["a","b"],["c","d"]], word = "abcd"
+输出：false
+
+提示：
+
+1 <= board.length <= 200
+1 <= board[i].length <= 200
+board 和 word 仅由大小写英文字母组成
+
+链接：https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof
+Solution:
+https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/solution/mian-shi-ti-12-ju-zhen-zhong-de-lu-jing-shen-du-yo/
+"""
+from typing import List
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def dfs(i, j, k):
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]:
+                return False
+            if k == len(word) - 1:
+                return True
+            board[i][j] = ''
+            res = dfs(i - 1, j, k + 1) or dfs(i, j - 1, k + 1) or dfs(i + 1, j, k + 1) or dfs(i, j + 1, k + 1)
+            board[i][j] = word[k]
+            return res
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0):
+                    return True
+        return False
+
+
+b, w = [["a", "b"], ["c", "d"]], "abcd"
+s = Solution()
+print(s.exist(board=b, word=w))
