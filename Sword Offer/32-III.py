d0176dfef2816cd64ec72dde85ee8c6d5252e711
handsome-fish <gitfish@163.com>
2021-06-14 18:10:35 +0800

    BFS, use queue

---
+++ Sword Offer/32-III.py
@@ -0,0 +1,53 @@
+"""
+剑指 Offer 32 - III. 从上到下打印二叉树 III
+
+请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+返回其层次遍历结果：
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
+提示：
+节点总数 <= 1000
+"""
+from typing import List
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, queue = [], deque()
+        queue.append(root)
+        reverse = False
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                tmp.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            if reverse:
+                tmp = tmp[::-1]
+            reverse = not reverse
+            res.append(tmp)
+        return res
